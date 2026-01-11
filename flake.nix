0f7b04ce312104813f05d3cd6e5c9beacfa79257
benj9000 <49900294+benj9000@users.noreply.github.com>
2026-01-11 23:36:35 +0100

    Add outputs to flake

--- flake.nix
+++ flake.nix
@@ -7,13 +7,39 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+    {
+      homeManagerModules.default = import ./nix/hm-module.nix self;
+    } // flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        applicationName = "git-worktree-manager";
+        dependencies = with pkgs; [ git fzf gawk gnugrep ];
+        package = pkgs.writeShellApplication {
+          name = applicationName;
+          runtimeInputs = dependencies;
+          text = builtins.readFile ./src/git-worktree-manager;
+        };
       in
       {
+        packages = rec {
+          default = git-worktree-manager;
+          git-worktree-manager = package;
+        };
+
+        apps = rec {
+          default = git-worktree-manager;
+          git-worktree-manager = flake-utils.lib.mkApp {
+            drv = package;
+            exePath = "/bin/${applicationName}";
+          };
+        };
+
         devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ git ];
+          nativeBuildInputs = dependencies;
+          shellHook = /*sh*/ ''
+            export PATH="$PWD/src:$PATH";
+          '';
         };
       }
     );
